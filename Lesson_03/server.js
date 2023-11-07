ca893a84c3a82a5f09de0a20e2d7bb597e0ba8ff
Hoe Ze Il <anbschool0026@026s-Mac-mini.local>
2023-11-07 08:12:28 +0700

    Redux Query

--- Lesson_03/server.js
+++ Lesson_03/server.js
@@ -1,17 +1,15 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const path = require('path');
-const { logger, logEvents } = require('./middleware/logger');
-const errorHandler = require('./middleware/errorHandler');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
-const connectDB = require('./config/dbConn');
-const mongoose = require('mongoose');
+const path = require("path");
+const { logger, logEvents } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3500;
-
-console.log(process.env.NODE_ENV);
 
 connectDB();
 
@@ -23,32 +21,32 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use("/", express.static(path.join(__dirname, "public")));
 
-app.use('/', require('./routes/root'));
+app.use("/", require("./routes/root"));
 
-app.all('*', (req, res) => {
+app.all("*", (req, res) => {
     res.status(404);
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    } else if (req.accepts('json')) {
-        res.json({ message: '404 Not Found' });
+    if (req.accepts("html")) {
+        res.sendFile(path.join(__dirname, "views", "404.html"));
+    } else if (req.accepts("json")) {
+        res.json({ message: "404 Not Found" });
     } else {
-        res.type('txt').send('404 Not Found');
+        res.type("txt").send("404 Not Found");
     }
 });
 
 app.use(errorHandler);
 
-mongoose.connection.once('open', () => {
-    console.log('connect to MongoDB');
+mongoose.connection.once("open", () => {
+    console.log("connect to MongoDB");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
 
-mongoose.connection.on('error', (err) => {
+mongoose.connection.on("error", (err) => {
     console.log(err);
     logEvents(
         `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
-        'mongoErrLog.log'
+        "mongoErrLog.log"
     );
 });
--- Lesson_04/models/Note.js
+++ Lesson_04/models/Note.js
@@ -16,7 +16,6 @@ const noteSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
-
         completed: {
             type: Boolean,
             default: false,
@@ -26,5 +25,11 @@ const noteSchema = new mongoose.Schema(
         timestamps: true,
     }
 );
+
+noteSchema.plugin(AutoIncrement, {
+    inc_field: "ticket",
+    id: "ticketNums",
+    start_seq: 500,
+});
 
 module.exports = mongoose.model("Note", noteSchema);
